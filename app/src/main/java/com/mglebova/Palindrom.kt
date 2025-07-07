ca57493e35d10d9fef424c0efc0efd3b71d09927
Alex <alez.grante@gmail.com>
2025-07-07 22:47:36 +0300

    add tasks

---
+++ app/src/main/java/com/mglebova/Palindrom.kt
@@ -0,0 +1,18 @@
+package com.mglebova
+
+fun main() {
+    val number1 = 9008009
+    val number2 = 70080099
+
+    val b1 = isPalindrome(number1)
+    val b2 = isPalindrome(number2)
+
+    println(b1)
+    println(b2)
+}
+
+
+fun isPalindrome(x: Int): Boolean {
+
+    return false
+}
---
+++ app/src/main/java/com/mglebova/ReverseInt.kt
@@ -0,0 +1,17 @@
+package com.mglebova
+
+fun main() {
+
+    val number = 46738
+
+    val reversedNumber = reverse(number)
+
+    println(reversedNumber)
+
+}
+
+fun reverse(x: Int): Int {
+
+
+    return -1
+}
