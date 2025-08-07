a5d686afd668edc1cca2a016b45afa4b7b1c91c1
Maryana <marydelreyss@gmail.com>
2025-08-07 16:11:21 +0300

    ALG-19: SumOfSquaresOfUniqueNumbersVr2

--- app/src/main/java/com/mglebova/SumOfSquaresOfUniqueNumbers.kt
+++ app/src/main/java/com/mglebova/SumOfSquaresOfUniqueNumbers.kt
@@ -1,9 +1,9 @@
 package com.mglebova
 
 fun main() {
-    val input = "7 5 3 3" // строка с целыми числами, которые разделены пробелами
-    val actual = sumOfSquaresOfUniqueNumbers(input)
-    val exptected = 83
+    val input = "привет 7 мир 5 7 3" // строка с целыми числами, которые разделены пробелами
+    val actual = sumOfSquaresOfUniqueNumbersVr2(input)
+    val exptected = listOf(9,25,49)
 
     // суть (7*7) + (5*5) + (3*3) = 83 - берем только уникальные значения
 
@@ -12,12 +12,12 @@ fun main() {
 }
 
 
-fun sumOfSquaresOfUniqueNumbers(string: String): Int {
+fun sumOfSquaresOfUniqueNumbersVr2(string: String): List <Int> {
     return string
         .split(" ")
-        .map { it.toInt() }
+        .map { it.toIntOrNull() }
+        .filterNotNull()
         .distinct()
         .map { it*it }
-        .sum()
-
+        .sortedDescending()
 }
