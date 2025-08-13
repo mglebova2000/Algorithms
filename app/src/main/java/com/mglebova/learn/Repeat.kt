d89334c7f8355ce0dca13479c41152b6cc8362d6
Maryana <marydelreyss@gmail.com>
2025-08-13 20:31:22 +0300

    class

--- app/src/main/java/com/mglebova/learn/Repeat.kt
+++ app/src/main/java/com/mglebova/learn/Repeat.kt
@@ -1,13 +1,30 @@
 fun main() {
-    val capitals = mapOf("Italy" to "Rome", "Canada" to "Ottawa")
+    val alex = Personn("Alex", "Glebob")
+    val alex2 = Personn("Alex", "Glebob")
 
-    val el = capitals["Spain"]
-
-    val length = el?.length ?: -2
-
-    println(length)
+    Machine.width = 200
+    Machine.width = 300
+    Machine.width = 400
 
 
+    alex2.setAge(2)
 
 }
+class Personn(val name: String, val surName: String) {
+    private var age = 10
 
+    fun setAge(value: Int) {
+
+        age = value
+    }
+}
+
+object Machine {
+
+    var width = 200
+
+    fun cry() {
+
+    }
+
+}
