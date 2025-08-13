b45ed7d295652970ebc939e9a19143bc3bcfa309
Maryana <marydelreyss@gmail.com>
2025-08-13 18:13:26 +0300

    Lesson3HFK

---
+++ app/src/main/java/com/mglebova/learn/Repeat.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val str = "fesmkfsenk"
+    val str2 = "fsefsefs"
+    val i = 3
+
+    val newnumber = i.incrementForMeryana()
+
+}
+
+fun String.privet(){
+
+
+}
+
+fun Int.incrementForMeryana() : Int{
+    return this + 1
+}
+
