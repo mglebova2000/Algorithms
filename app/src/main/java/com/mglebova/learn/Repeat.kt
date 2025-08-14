d154b55b482d7d496a89868cd6d438601287008e
Maryana <marydelreyss@gmail.com>
2025-08-14 17:21:01 +0300

    FizzBuzz.kt

--- app/src/main/java/com/mglebova/learn/Repeat.kt
+++ app/src/main/java/com/mglebova/learn/Repeat.kt
@@ -2,15 +2,14 @@ fun main() {
     val alex = Personn("Alex", "Glebob")
     val alex2 = Personn("Alex", "Glebob")
 
-    Machine.width = 200
-    Machine.width = 300
-    Machine.width = 400
-
+    alex.peremennaia
 
     alex2.setAge(2)
 
 }
 class Personn(val name: String = DEFAULT_NAME, val surName: String) {
+    lateinit var peremennaia : String
+
     private var age = DEFAULT_AGE
 
     fun setAge(value: Int) {
