b891970aa202b5b4fd475380ca4e334b9c87d26c
Alex <alez.grante@gmail.com>
2025-08-29 22:07:30 +0300

    ALG-31: generateFizzBuzz2

---
+++ app/src/main/java/com/mglebova/interviev/FizzBuzz2.kt
@@ -0,0 +1,42 @@
+// Написать программу, которая генерирует последовательность от 1 до N, где:
+// числа, кратные 3, заменяются на "Fizz",
+//числа, кратные 5 — на "Buzz",
+//числа, кратные и 3, и 5 — на "FizzBuzz".
+
+fun main() {
+    val actual = generateFizzBuzz2(15)
+    val exptected = listOf(
+        "1",
+        "2",
+        "Fizz",
+        "4",
+        "Buzz",
+        "Fizz",
+        "7",
+        "8",
+        "Fizz",
+        "Buzz",
+        "11",
+        "Fizz",
+        "13",
+        "14",
+        "FizzBuzz"
+    )
+
+    println("actual=$actual")
+    println("exptected=$exptected")
+}
+
+fun generateFizzBuzz2(n: Int): List<String> {
+    val list = mutableListOf<String>()
+    for (i in 1..n) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            list.add("FizzBuzz")
+        } else if (i % 3 == 0) {
+            list.add("Fizz")
+        } else if (i % 5 == 0) {
+            list.add("Buzz")
+        } else list.add("$i")
+    }
+    return list
+}
